ac52df551b058588a77e05180f97fc60d8ce3a04
Yerovy Espitia <luyeesgar@gmail.com>
2024-07-22 18:59:09 -0500

    feat: add forum route

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 import { gameRoute } from './routes/game.routes'
+import { postRoute } from './routes/forum.routes'
 
 const app = new Hono()
 
 app.get('*', logger())
 
-app.basePath('/api').route('/game', gameRoute)
+app.basePath('/api').route('/game', gameRoute).route('/forum', postRoute)
 
 export default app
