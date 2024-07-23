762ae394c0dfe9ce3fbd13a09937baf4322af9bb
Yerovy Espitia <luyeesgar@gmail.com>
2024-07-22 19:01:02 -0500

    feat: add postSchema + fix: change validation schema of arrays

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -7,11 +7,22 @@ export const gameSchema = z.object({
   title: z.string().min(3),
   wallpaper: z.string().url(),
   description: z.string().min(20),
-  genre: z.string().array().min(1),
-  platforms: z.string().array().min(1),
+  genre: z.array(z.string()).min(1),
+  platforms: z.array(z.string()).min(1),
   releaseDate: z.date(),
   createdAt: z.date(),
-  developers: z.string().array().min(1),
-  publishers: z.string().array().min(1),
+  developers: z.array(z.string()).min(1),
+  publishers: z.array(z.string()).min(1),
   rating: z.number().default(0),
+})
+
+export const postSchema = z.object({
+  id: idSchema,
+  title: z.string().min(3),
+  description: z.string().min(3),
+  tags: z.array(z.enum(['Adventure', 'Fantasy'])).min(1),
+  userId: idSchema,
+  username: z.string().min(3),
+  userPicture: z.string().url(),
+  createdAt: z.date(),
 })
